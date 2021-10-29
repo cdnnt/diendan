113ac0852394e5f0fcf5cadd80440d41d20d675d
Nguyễn Minh Đức <79192823+nmd260304@users.noreply.github.com>
2021-10-29 17:03:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'localhost';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'toihuong_tuoitre';
+$config['db']['password'] = 'muong1992';
+$config['db']['dbname'] = 'toihuong_tuoitre';
+
+$config['fullUnicode'] = true;
